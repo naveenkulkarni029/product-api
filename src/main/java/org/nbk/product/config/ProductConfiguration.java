e4e3c3e5eed70fc9da747206d64e2d2321777b7f
Naveen Kulkarni <naveenkulkarni029@gmail.com>
2019-10-16 10:18:05 +0530

    updated config and code

--- src/main/java/org/nbk/product/config/ProductConfiguration.java
+++ src/main/java/org/nbk/product/config/ProductConfiguration.java
@@ -2,12 +2,6 @@ package org.nbk.product.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.MongoDbFactory;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
-import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
-import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
-import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
